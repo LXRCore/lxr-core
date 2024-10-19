8b883c7a5e834647b6dc3680d457e4340c1dfb1f
Master Mind <48193931+iboss21@users.noreply.github.com>
2024-10-19 00:03:05 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,13 @@
 fx_version 'cerulean'
 game 'rdr3'
+
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 description 'LXRCore RedM Edition - Fully converted and maintained by iBoss (https://github.com/iboss21)'
 version '1.0.3'
 
 shared_scripts {
-    'shared/locale.lua', -- Ensure that shared scripts are loaded first
-    'config.lua',
+    'config.lua',            -- Remove the locale reference since it's not needed
     'shared/main.lua',
     'shared/items.lua',
     'shared/jobs.lua',
@@ -28,7 +28,6 @@ client_scripts {
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
-    'server/locales/en.lua', -- Add your language file here
     'server/debug.lua',
     'server/functions.lua',
     'server/player.lua',
